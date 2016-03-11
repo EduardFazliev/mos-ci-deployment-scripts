edddbe419187f07a3677da0bdaffca3bd2d99d2d
efazliev <efazliev@mera.ru>
2016-03-11 17:06:01 +0300

    add new python script for init host

---
+++ jenkins-job-builder/python_scripts/9.0_prepare_host/9.0_prepare_host.py
@@ -0,0 +1,16 @@
+import os
+
+
+def get_env_variables():
+
+
+
+def main():
+    get_env_variables()
+    iso_dir = os.environ['ISO_DIR']
+    snapshot_name = os.environ['SNAPSHOT_NAME']
+    env_name =
+
+
+if __name__ == '__main__':
+    main()
