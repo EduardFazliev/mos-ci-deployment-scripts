648fcb774ba2981dd98a23b575b791e89610e605
efazliev <efazliev@mera.ru>
2016-05-05 17:46:16 +0300

    Remove "'" from tox_based template.

--- jenkins-job-builder/shell_scripts/tox_based_tests.sh
+++ jenkins-job-builder/shell_scripts/tox_based_tests.sh
@@ -17,5 +17,5 @@ virtualenv venv
 . venv/bin/activate
 pip install -U pip
 pip install tox
-tox -e '{tox_test_name}' -- -v -E "$ENV_NAME" -S "$SNAPSHOT_NAME"
+tox -e {tox_test_name} -- -v -E "$ENV_NAME" -S "$SNAPSHOT_NAME"
 deactivate
