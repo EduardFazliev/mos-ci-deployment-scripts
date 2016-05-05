4c2b83c0e7bcbe46fed826dd183746d1c57fce3b
efazliev <efazliev@mera.ru>
2016-05-05 21:23:22 +0300

    Fix.

--- jenkins-job-builder/shell_scripts/deploy_template_env.sh
+++ jenkins-job-builder/shell_scripts/deploy_template_env.sh
@@ -3,7 +3,7 @@
 # This script deploy MirantisOpenStak from templates
 
 
-patch_fuel_qa(){
+patch_fuel_qa(){{
 # Check and apply patch to fuel_qa
 set +e
 pushd $PWD/fuel-qa
@@ -17,7 +17,7 @@ git apply $patch_file
 fi
 popd
 set -e
-}
+}}
 
 
 
@@ -45,7 +45,7 @@ exit 1
 fi
 
 PWD=$(pwd)
-#CONFIG_PATH=${1:?You should pass a valid path to Yaml tempate as first argument}
+#CONFIG_PATH=${{1:?You should pass a valid path to Yaml tempate as first argument}}
 
 if [ ! -f "$CONFIG_PATH" ]; then
 echo "$CONFIG_PATH is not exists or not a regular file"
@@ -53,49 +53,49 @@ exit 1
 fi
 CONFIG_FOLDER=$(basename $(dirname $CONFIG_PATH))
 CONFIG_FILE=$(basename $CONFIG_PATH)
-CONFIG_NAME="${CONFIG_FILE%.*}"
+CONFIG_NAME="${{CONFIG_FILE%.*}}"
 
 SNAPSHOT_NAME="$CONFIG_FOLDER"_"$CONFIG_NAME"
-echo "SNAPSHOT_NAME=${SNAPSHOT_NAME}" >> "$ENV_INJECT_PATH"
+echo "SNAPSHOT_NAME=${{SNAPSHOT_NAME}}" >> "$ENV_INJECT_PATH"
 
 # Set fuel QA version
 # https://github.com/openstack/fuel-qa/branches
-FUEL_QA_VER=${FUEL_QA_VER:-'master'}
+FUEL_QA_VER=${{FUEL_QA_VER:-'master'}}
 
 V_ENV_DIR="$(pwd)/fuel-devops-venv"
 
 # set ENV_NAME if it is not defined
-ENV_NAME=${ENV_NAME:-"$CONFIG_FOLDER"_"$CONFIG_NAME"_"$RANDOM"}
-DISABLE_SSL=${DISABLE_SSL:-TRUE}
-KVM_USE=${KVM_USE:-true}
-INTERFACE_MODEL=${INTERFACE_MODEL:-virtio}
-PLUGINS_CONFIG_PATH=${PLUGINS_CONFIG_PATH:-$(pwd)/plugins.yaml}
+ENV_NAME=${{ENV_NAME:-"$CONFIG_FOLDER"_"$CONFIG_NAME"_"$RANDOM"}}
+DISABLE_SSL=${{DISABLE_SSL:-TRUE}}
+KVM_USE=${{KVM_USE:-true}}
+INTERFACE_MODEL=${{INTERFACE_MODEL:-virtio}}
+PLUGINS_CONFIG_PATH=${{PLUGINS_CONFIG_PATH:-$(pwd)/plugins.yaml}}
 # more time can be required to deploy env
-DEPLOYMENT_TIMEOUT=${DEPLOYMENT_TIMEOUT:-10000}
+DEPLOYMENT_TIMEOUT=${{DEPLOYMENT_TIMEOUT:-10000}}
 
 export ENV_NAME DISABLE_SSL KVM_USE INTERFACE_MODEL PLUGINS_CONFIG_PATH DEPLOYMENT_TIMEOUT
 
-echo "Env name:         ${ENV_NAME}"
-echo "Snapshot name:    ${SNAPSHOT_NAME}"
-echo "Fuel QA branch:   ${FUEL_QA_VER}"
+echo "Env name:         ${{ENV_NAME}}"
+echo "Snapshot name:    ${{SNAPSHOT_NAME}}"
+echo "Fuel QA branch:   ${{FUEL_QA_VER}}"
 echo ""
 
 # Check if folder for virtual env exist
-if [ ! -d "${V_ENV_DIR}" ]; then
-virtualenv --no-site-packages ${V_ENV_DIR}
+if [ ! -d "${{V_ENV_DIR}}" ]; then
+virtualenv --no-site-packages ${{V_ENV_DIR}}
 fi
 
-source ${V_ENV_DIR}/bin/activate
+source ${{V_ENV_DIR}}/bin/activate
 pip install -U pip
 
 # Check if fuel-qa folder exist
 if [ ! -d fuel-qa ]; then
-git clone -b "${FUEL_QA_VER}" https://github.com/openstack/fuel-qa
+git clone -b "${{FUEL_QA_VER}}" https://github.com/openstack/fuel-qa
 else
 pushd fuel-qa
 git clean -f -d -x
 git checkout -- *
-git checkout "${FUEL_QA_VER}"
+git checkout "${{FUEL_QA_VER}}"
 git reset --hard
 git pull
 popd
@@ -116,10 +116,10 @@ cp $CONFIG_PATH fuel-qa/system_test/tests_templates/tests_configs/
 cd fuel-qa
 
 
-if [ "${INTERFACE_MODEL}" == 'virtio' ]; then
+if [ "${{INTERFACE_MODEL}}" == 'virtio' ]; then
 # Virtio network interfaces have names eth0..eth5
 # (rather than default names - enp0s3..enp0s8)
-for i in {0..5}; do
+for i in {{0..5}}; do
 export IFACE_$i=eth$i
 done
 fi
@@ -128,6 +128,6 @@ fi
 ./run_system_test.py run 'system_test.deploy_env' --with-config $CONFIG_NAME
 
 # make snapshot if deployment is successful
-dos.py suspend ${ENV_NAME}
-dos.py snapshot ${ENV_NAME} ${SNAPSHOT_NAME}
-dos.py resume ${ENV_NAME}
+dos.py suspend ${{ENV_NAME}}
+dos.py snapshot ${{ENV_NAME}} ${{SNAPSHOT_NAME}}
+dos.py resume ${{ENV_NAME}}
