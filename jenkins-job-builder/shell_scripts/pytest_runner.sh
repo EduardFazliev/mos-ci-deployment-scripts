2213bbbb96f7ad3d63f5a036b2d431548c56a75a
efazliev <efazliev@mera.ru>
2016-05-05 20:42:52 +0300

    Add neutron single job template

---
+++ jenkins-job-builder/shell_scripts/pytest_runner.sh
@@ -0,0 +1,12 @@
+rm -rf mos-integration-tests
+git clone https://github.com/Mirantis/mos-integration-tests.git
+cd mos-integration-tests
+
+virtualenv tests
+. tests/bin/activate
+
+pip install -U pip
+pip install -r requirements.txt
+py.test {test_path} -E "$ENV_NAME" -S "$SNAPSHOT_NAME" -v
+
+deactivate
