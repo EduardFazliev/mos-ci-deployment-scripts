628009557cd647cf4745c25291f3a695292d3c16
efazliev <efazliev@mera.ru>
2016-05-12 21:10:52 +0300

    Remove Ironic proxy start during the depoy.

--- jenkins-job-builder/shell_scripts/template_scripts/tox_based_test_runner.sh
+++ jenkins-job-builder/shell_scripts/template_scripts/tox_based_test_runner.sh
@@ -1,12 +1,5 @@
 set +e
 
-if [[ {name} == 'Ironic' ]]; then
-wget https://raw.githubusercontent.com/EduardFazliev/mos-ci-deployment-scripts/feature/jjb/jenkins-job-builder/python_scripts/ironic/proxy.py
-nohup python proxy.py  &>proxy.log </dev/null &
-PID=`echo $!`
-echo "PROXY_PID=$PID" >> "$ENV_INJECT_PATH"
-fi
-
 rm -rf mos-integration-tests
 git clone https://github.com/Mirantis/mos-integration-tests.git
 cd mos-integration-tests
@@ -31,9 +24,5 @@ sudo cp *.log "$REPORT_PREFIX"/"$ENV_NAME"_"$SNAPSHOT_NAME" \
 deactivate
 
 sudo dos.py destroy "$ENV_NAME"
-
-if [[ {name} == 'Ironic' ]]; then
-sudo kill -9 "$PROXY_PID" || true
-fi
 
 exit 0
